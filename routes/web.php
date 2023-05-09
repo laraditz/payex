1f8f072f10491b560c9e3a04832054d2e124c485
Farhan <raditzfarhan@gmail.com>
2023-05-10 06:54:17 +0800

    Fix route callback

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use Laraditz\Payex\Http\Controllers\PayexController;
 
 Route::match(['get', 'post'], '/done', [PayexController::class, 'done'])->name('payex.done');
-Route::post('/callback', [PayexController::class, 'backend'])->name('payex.callback');
+Route::post('/callback', [PayexController::class, 'callback'])->name('payex.callback');
